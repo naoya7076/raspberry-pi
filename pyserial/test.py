b9519bb2abf239037bcd16b682f471219c75b37d
Naoya Shimizu <naoya7076@gmail.com>
2018-12-07 15:04:12 +0900

    update for PC

--- pyserial/test.py
+++ pyserial/test.py
@@ -2,6 +2,6 @@ import serial
 # PORT1:!
 # PORT2: "
 # PORT3: #
-ser = serial.Serial(port="/dev/ttyUSB0", baudrate=9600)
+ser = serial.Serial(port="/dev/ttyUSB0", baudrate=9600)	#PCのUSBは"COM3"
 ser.write(b'C" 39000\r')
 ser.close()
